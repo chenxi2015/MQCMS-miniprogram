aab6c35144f619ef1f716972954d4d72d60e8d16
chenxi <bbxycx.18@163.com>
2019-12-03 13:51:30 +0800

    init

--- chameleon.config.js
+++ chameleon.config.js
@@ -1,8 +1,8 @@
 
 // 设置静态资源的线上路径
-const publicPath = '//www.static.chameleon.com/cml';
+const publicPath = '//www.mqcms.net/';
 // 设置api请求前缀
-const apiPrefix = 'https://api.chameleon.com';
+const apiPrefix = 'https://api.mqcms.net';
 
 cml.config.merge({
   templateLang: "cml",
